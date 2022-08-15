75500fa452b7f2df18dd8baeb3dcf228037a1704
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-08-14 23:03:27 -0300

    feat: adicionando responsividade

--- Pokemon-Card/src/pages/Home/index.tsx
+++ Pokemon-Card/src/pages/Home/index.tsx
@@ -1,30 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { Container, Box } from "@mui/material";
+import React, { useState } from "react";
+import { Container, Box, useMediaQuery } from "@mui/material";
 import { List, Details } from "../../components";
 import { Pokemon } from "../../types/@types";
 
 const Home: React.FC = () => {
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
   const [pikachuCounter, setPikachuCounter] = useState(0);
-  
+  const matches = useMediaQuery("(min-width:950px)");
+  console.log(matches);
+
   return (
     <Container
       fixed
       sx={{
-        backgroundImage: pikachuCounter >= 3 ?
-          "url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg)" : null
+        backgroundImage:
+          pikachuCounter >= 3
+            ? "url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg)"
+            : null,
       }}
     >
       <Box
         display="flex"
         justifyContent={"center"}
         alignItems={"center"}
-        sx={{ height: "100vh", width: "100vh" }}
+        height={matches ? "100vh" : "150vh"}
+        width={matches ? "100vh" : "50vh"}
+        flexDirection={!matches ? "column-reverse" : "row"}
+        component={"div"}
       >
-        <Box marginX={1}>
+        <Box margin={1}>
           <List setSelectedPokemon={setSelectedPokemon} />
         </Box>
-        <Box marginX={1}>
+        <Box margin={1}>
           <Details
             selectedPokemon={selectedPokemon}
             pikachuCounter={pikachuCounter}
